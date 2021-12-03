9318c11f3c4952b27d7b3a6836f035991e617762
matteo9723 <matteolandri7@gmail.com>
2021-12-03 15:07:05 +0100

    aiuto

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,21 @@ export default {
     }
   },
 
+  mounted:{
+      popolari(){
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=9f9a8bc474e9c025cefdaf1ff2c51e03&language=en-US&page=1`).then( r =>{
+          this.listaFilm=r.data.results
+          console.log(r.data.results);
+        }).catch( e => {
+         console.log(e);
+        });
+        axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=9f9a8bc474e9c025cefdaf1ff2c51e03&language=en-US&page=1`).then( r =>{
+          this.listaSerie=r.data.results
+        }).catch( e => {
+         console.log(e);
+        });
+      },
+  },
   methods:{
     ricerca(value){
      
@@ -39,9 +54,9 @@ export default {
       }).catch( e => {
        console.log(e);
       });
-     
-    }
-  }
+    },
+  },
+
 
 
 }
